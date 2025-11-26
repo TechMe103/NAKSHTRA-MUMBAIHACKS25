1b35f16c9a1d6ee1171d4595dbbce6fcc121657e
Ritesh Sonawane <ritesh26live@gmail.com>
2025-11-26 18:01:28 +0530

    Restored old file of insightRoute.js

--- backend/routes/insightRoute.js
+++ backend/routes/insightRoute.js
@@ -1,42 +1,36 @@
 import express from "express";
-// FIX: Changed import to the correct file name and correct function name
-import { protectRoute } from "../middlewares/authMiddleware.js";
+import { auth } from "../middlewares/auth.js";
 
 import {
-  createRecord,
-  getRecords,
-  getRecordsByType,
-  updateRecord,
-  deleteRecord,
-  markInsightRead,
+  createRecord,
+  getRecords,
+  getRecordsByType,
+  updateRecord,
+  deleteRecord,
+  markInsightRead,
 } from "../controllers/insightController.js";
 
 import { validateFinancialRecord } from "../validators/insightValidation.js";
 
 const router = express.Router();
 
-// FIX: Replaced 'auth' with 'protectRoute' in all lines below
-
 // CREATE
-router.post("/", protectRoute, validateFinancialRecord, createRecord);
+router.post("/", auth, validateFinancialRecord, createRecord);
 
 // GET ALL
-router.get("/", protectRoute, getRecords);
-router.get("/", protectRoute, getRecords);
+router.get("/", auth, getRecords);
 
 // GET BY TYPE
-router.get("/type/:type", protectRoute, getRecordsByType);
-router.get("/type/:type", protectRoute, getRecordsByType);
+router.get("/type/:type", auth, getRecordsByType);
 
 // UPDATE
-router.put("/:id", protectRoute, validateFinancialRecord, updateRecord);
+router.put("/:id", auth, validateFinancialRecord, updateRecord);
 
 // DELETE
-router.delete("/:id", protectRoute, deleteRecord);
-router.delete("/:id", protectRoute, deleteRecord);
+router.delete("/:id", auth, deleteRecord);
 
 // MARK INSIGHT AS READ
-router.patch("/:id/read", protectRoute, markInsightRead);
-router.patch("/:id/read", protectRoute, markInsightRead);
+router.patch("/:id/read", auth, markInsightRead);
 
 export default router;
+//insightRoute.js
